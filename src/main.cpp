a59ba27b282be649bf5b353f132f71c78d7a8a51
findeevy <findeevy@gmail.com>
2025-05-21 20:05:56 -0500

    Set up some more instance creation stuff!

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ class HelloTriangleApplication{
       cleanup();
     }
   private:
+    VkInstance instance;
     GLFWwindow* window;
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
@@ -24,7 +25,40 @@ class HelloTriangleApplication{
 
       window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
+    void createInstance(){
+      //Info to set up the Vulkan application.
+      VkApplicationInfo appInfo{};
+      appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+      appInfo.pApplicationName = "Finengine";
+      appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+      appInfo.pEngineName = "No Engine";
+      appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+      appInfo.apiVersion = VK_API_VERSION_1_0;
+      
+      //Info to set up the Vulkan driver's extensions and validation layers.
+      VkInstanceCreateInfo createInfo{};
+      createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+      
+      //Pipe our application info into the creation info via a pointer.
+      createInfo.pApplicationInfo = &appInfo;
+
+      //Window handling.
+      uint32_t glfwExtensionCount = 0;
+      const char** glfwExtensions;
+      glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+      createInfo.enabledExtensionsCount = glfwExtensionCount;
+      createInfo.ppEnabledExtensionNames = glfwExtensions;
+      createInfo.enabledLayerCount 0;
+      VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+      if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS){
+        throw std::runtime_error("failed to create instance!");
+      }
+
+      //Check for extension support.
+
+    }
     void initVulkan(){
+      createInstance();
     }
     void mainLoop(){
       while(!glfwWindowShouldClose(window)){
