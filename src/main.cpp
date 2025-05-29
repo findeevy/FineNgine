4af7762401a6909c96383abddd3a35118b2a0a6b
findeevy <findeevy@gmail.com>
2025-05-28 19:08:11 -0500

    Finished up validation layers.

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,13 @@ class HelloTriangleApplication{
     const bool enableValidationLayers = true;
   #endif
     
+    void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator){
+      auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+      if (func != nullptr){
+        func(instance, debugMessenger, pAllocator);
+      }
+    }
+
     void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
       createInfo = {};
       createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
@@ -38,6 +45,18 @@ class HelloTriangleApplication{
       createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
       createInfo.pfnUserCallback = debugCallback;
     }
+
+    void setupDebugMessenger() {
+      if (!enableValidationLayers) {
+        return;
+      }
+      VkDebugUtilsMessengerCreateInfoEXT createInfo;
+      populateDebugMessengerCreateInfo(createInfo);
+      if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
+        throw std::runtime_error("failed to set up debug messenger!");
+      }
+    }
+
 
     static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData){
       std::cerr << "Validation Layer: " << pCallbackData->pMessage << std::endl;
@@ -89,16 +108,6 @@ class HelloTriangleApplication{
       return VK_ERROR_EXTENSION_NOT_PRESENT;
     }
 
-    void setupDebugMessenger() {
-      if (!enableValidationLayers) return;
-        VkDebugUtilsMessengerCreateInfoEXT createInfo;
-        populateDebugMessengerCreateInfo(createInfo);
-
-        if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
-          throw std::runtime_error("failed to set up debug messenger!");
-      }
-    }
-
     void initWindow(){
       glfwInit();
 
@@ -128,13 +137,26 @@ class HelloTriangleApplication{
       //Pipe our application info into the creation info via a pointer.
       createInfo.pApplicationInfo = &appInfo;
 
-      //Window handling.
+      //Extension handling.
       auto glfwExtensions = getRequiredExtensions();
       createInfo.enabledExtensionCount = static_cast<uint32_t>(glfwExtensions.size());
       createInfo.ppEnabledExtensionNames = glfwExtensions.data();
-      createInfo.enabledLayerCount =  0;
-      VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
-      if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS){
+
+      VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+      if (enableValidationLayers) {
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+
+        populateDebugMessengerCreateInfo(debugCreateInfo);
+        createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
+      } 
+      else {
+        createInfo.enabledLayerCount = 0;
+
+        createInfo.pNext = nullptr;
+      }
+
+      if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
         throw std::runtime_error("failed to create instance!");
       }
 
@@ -171,6 +193,9 @@ class HelloTriangleApplication{
       }
     }
     void cleanup(){
+      if (enableValidationLayers){
+        DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+      }
       glfwDestroyWindow(window);
       glfwTerminate();
     }
