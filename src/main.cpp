d2b081466ef1f9b905f1079a657f58e8cf892760
findeevy <findeevy@gmail.com>
2025-05-29 19:32:51 -0500

    logical device selection

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #define GLFW_INCLUDE_VULKAN
+#define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 #include <vector>
 #include <cstring>
+#include <optional>
 
 class HelloTriangleApplication{
   public:
@@ -20,6 +24,10 @@ class HelloTriangleApplication{
     GLFWwindow* window;
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device;
+    VkQueue graphicsQueue;
+    VkSurfaceKHR surface;
 
     const std::vector<const char*> validationLayers = {
       "VK_LAYER_KHRONOS_validation"
@@ -31,6 +39,35 @@ class HelloTriangleApplication{
     const bool enableValidationLayers = true;
   #endif
     
+    struct QueueFamilyIndices{
+      std::optional<uint32_t> graphicsFamily;
+      bool isComplete() {
+        return graphicsFamily.has_value();
+      }
+    }
+    
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+      QueueFamilyIndices indices;
+      uint32_t queueFamilyCount = 0;
+      vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+      std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+      vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+      int i = 0;
+      for (const auto& queueFamily : queueFamilies) {
+        if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+          indices.graphicsFamily = i;
+        }
+        if (indices.isComplete()) {
+          break;
+        }
+        i++;
+      }
+      return indices;
+
+    }
+
     void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator){
       auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
       if (func != nullptr){
@@ -183,11 +220,8 @@ class HelloTriangleApplication{
     }
     
     bool isDeviceSuitable(VkPhysicalDevice device) {
-      VkPhysicalDeviceProperties deviceProperties;
-      VkPhysicalDeviceFeatures deviceFeatures;
-      vkGetPhysicalDeviceProperties(device, &deviceProperties);
-      vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
-      return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && deviceFeatures.geometryShader;
+      QueueFamilyIndices indices = findQueueFamilies(device);
+      return indices.isComplete();
     }
     
     //Maybe later allow the user to change GPU?
@@ -216,13 +250,50 @@ class HelloTriangleApplication{
       setupDebugMessenger();
       pickGraphicsCard();
     }
+    
+    void createLogicalDevice(){
+      QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+      VkDeviceQueueCreateInfo queueCreateInfo{};
+      queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+      queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+      queueCreateInfo.queueCount = 1;
+
+      float queuePriority = 1.0f;
+      queueCreateInfo.pQueuePriorities = &queuePriority;
+
+      VkPhysicalDeviceFeatures deviceFeatures{};
+
+      VkDeviceCreateInfo createInfo{};
+      createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+      createInfo.pQueueCreateInfos = &queueCreateInfo;
+      createInfo.queueCreateInfoCount = 1;
+      createInfo.pEnabledFeatures = &deviceFeatures;
+       createInfo.enabledExtensionCount = 0;
+
+      if (enableValidationLayers){
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+      } 
+      else{
+        createInfo.enabledLayerCount = 0;
+      }
+
+      if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS){
+        throw std::runtime_error("Failed to create logical device!");
+      }
+
+      vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+    }
 
     void mainLoop(){
       while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
       }
     }
+
     void cleanup(){
+      vkDestroyDevice(device, nullptr);
       if (enableValidationLayers){
         DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
       }
@@ -233,7 +304,6 @@ class HelloTriangleApplication{
 
 int main(){
   HelloTriangleApplication app;
-
   try {
     app.run();
   } catch (const std::exception& e){
