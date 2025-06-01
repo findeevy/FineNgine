02fcbb9b390a69619bbd62e21e155b4d426649b3
findeevy <findeevy@gmail.com>
2025-05-31 19:59:49 -0500

    finished up swapchain!

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@
 #include <cstring>
 #include <optional>
 #include <set>
+#include <cstdint>
+#include <limits>
+#include <algorithm>
+
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -81,6 +85,11 @@ class HelloTriangleApplication{
     VkQueue graphicsQueue;
     VkQueue presentQueue;
     VkSurfaceKHR surface;
+    
+    VkSwapchainKHR swapChain;
+    std::vector<VkImage> swapChainImages; 
+    VkFormat swapChainImageFormat;
+    VkExtent2D swapChainExtent;
     
     QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
       QueueFamilyIndices indices;
@@ -318,6 +327,8 @@ class HelloTriangleApplication{
       createInstance();
       setupDebugMessenger();
       pickGraphicsCard();
+      createLogicalDevice();
+      createSwapChain();
     }
     
     void createLogicalDevice(){
@@ -378,6 +389,55 @@ class HelloTriangleApplication{
       return availableFormats[0];
     }
 
+    void createSwapChain() {
+      SwapChainSupportDetails swapChainSupport = querySwapChainSupport(physicalDevice);
+      VkSurfaceFormatKHR surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats);
+      VkPresentModeKHR presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
+      VkExtent2D extent = chooseSwapExtent(swapChainSupport.capabilities);
+      uint32_t imageCount = swapChainSupport.capabilities.minImageCount + 1;
+
+      if (swapChainSupport.capabilities.maxImageCount > 0 && imageCount > swapChainSupport.capabilities.maxImageCount){
+            imageCount = swapChainSupport.capabilities.maxImageCount;
+      }
+
+      VkSwapchainCreateInfoKHR createInfo{};
+      createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+      createInfo.surface = surface;
+      createInfo.minImageCount = imageCount;
+      createInfo.imageFormat = surfaceFormat.format;
+      createInfo.imageColorSpace = surfaceFormat.colorSpace;
+      createInfo.imageExtent = extent;
+      createInfo.imageArrayLayers = 1;
+      createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+      QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+      uint32_t queueFamilyIndices[] = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+      if (indices.graphicsFamily != indices.presentFamily){
+        createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+        createInfo.queueFamilyIndexCount = 2;
+        createInfo.pQueueFamilyIndices = queueFamilyIndices;
+      } 
+      else{
+        createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+      }
+
+      createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+      createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+      createInfo.presentMode = presentMode;
+      createInfo.clipped = VK_TRUE;
+      createInfo.oldSwapchain = VK_NULL_HANDLE;
+
+      if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain) != VK_SUCCESS){
+        throw std::runtime_error("Failed to initialize swap chain.");
+      }
+
+      vkGetSwapchainImagesKHR(device, swapChain, &imageCount, nullptr);
+      swapChainImages.resize(imageCount);
+      vkGetSwapchainImagesKHR(device, swapChain, &imageCount, swapChainImages.data());
+      swapChainImageFormat = surfaceFormat.format;
+      swapChainExtent = extent;
+    }
+
     VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes){
       for (const auto& availablePresentMode : availablePresentModes){
         if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR){
@@ -413,6 +473,7 @@ class HelloTriangleApplication{
     }
 
     void cleanup(){
+      vkDestroySwapchainKHR(device,swapChain,nullptr);
       vkDestroyDevice(device, nullptr);
       if (enableValidationLayers){
         DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
