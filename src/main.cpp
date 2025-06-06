1ee8f36625d8372621f3ac7f214c383b75a6c1fd
findeevy <findeevy@gmail.com>
2025-06-05 20:53:23 -0500

    started work on drawing

--- src/main.cpp
+++ src/main.cpp
@@ -97,6 +97,10 @@ class HelloTriangleApplication{
     VkPipeline graphicsPipeline;
     std::vector<VkFramebuffer> swapChainFramebuffers;
     VkCommandPool commandPool;
+    VkCommandBuffer commandBuffer;
+    VkSemaphore imageAvailableSemaphore;
+    VkSemaphore renderFinishedSemaphore;
+    VkFence inFlightFence;
 
     QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
       QueueFamilyIndices indices;
@@ -347,6 +351,79 @@ class HelloTriangleApplication{
       createGraphicsPipeline();
       createFramebuffers();
       createCommandPool();
+      createCommandBuffer();
+      createSyncObjects();
+    }
+
+    void createSyncObjects(){
+      VkSemaphoreCreateInfo semaphoreInfo{};
+      semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+
+      VkFenceCreateInfo fenceInfo{};
+      fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+      fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+
+      if (vkCreateSemaphore(device, &semaphoreInfo, nullptr, &imageAvailableSemaphore) != VK_SUCCESS ||
+        vkCreateSemaphore(device, &semaphoreInfo, nullptr, &renderFinishedSemaphore) != VK_SUCCESS ||
+        vkCreateFence(device, &fenceInfo, nullptr, &inFlightFence) != VK_SUCCESS){
+        throw std::runtime_error("failed to create synchronization objects for a frame!");
+      }
+    }
+
+    void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex){
+      VkCommandBufferBeginInfo beginInfo{};
+      beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+      beginInfo.flags = 0;
+      beginInfo.pInheritanceInfo = nullptr;
+      if (vkBeginCommandBuffer(commandBuffer, &beginInfo) != VK_SUCCESS){
+        throw std::runtime_error("Failed to begin recording command buffer!");
+      }
+
+      VkRenderPassBeginInfo renderPassInfo{};
+      renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+      renderPassInfo.renderPass = renderPass;
+      renderPassInfo.framebuffer = swapChainFramebuffers[imageIndex];
+      renderPassInfo.renderArea.offset = {0, 0};
+      renderPassInfo.renderArea.extent = swapChainExtent;
+
+      VkClearValue clearColor = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
+      renderPassInfo.clearValueCount = 1;
+      renderPassInfo.pClearValues = &clearColor;
+
+      vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+      vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+
+      VkViewport viewport{};
+      viewport.x = 0.0f;
+      viewport.y = 0.0f;
+      viewport.width = (float) swapChainExtent.width;
+      viewport.height = (float) swapChainExtent.height;
+      viewport.minDepth = 0.0f;
+      viewport.maxDepth = 1.0f;
+      vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+
+      VkRect2D scissor{};
+      scissor.offset = {0, 0};
+      scissor.extent = swapChainExtent;
+      vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
+
+      vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+      vkCmdEndRenderPass(commandBuffer);
+
+      if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS){
+        throw std::runtime_error("Failed to record command buffer!");
+      }
+    }
+
+    void createCommandBuffer(){
+      VkCommandBufferAllocateInfo allocInfo{};
+      allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+      allocInfo.commandPool = commandPool;
+      allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+      allocInfo.commandBufferCount = 1;
+      if (vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to allocate command buffers!");
+      }
     }
 
     void createCommandPool() {
@@ -720,10 +797,19 @@ class HelloTriangleApplication{
     void mainLoop(){
       while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
+        drawFrame();
       }
     }
 
+    void drawFrame(){
+      vkWaitForFences(device, 1, &inFlightFence, VK_TRUE, UINT64_MAX);
+      vkResetFences(device, 1, &inFlightFence);
+    }
+
     void cleanup(){
+      vkDestroySemaphore(device, renderFinishedSemaphore, nullptr);
+      vkDestroySemaphore(device, imageAvailableSemaphore, nullptr);
+      vkDestroyFence(device, inFlightFence, nullptr);
       vkDestroyCommandPool(device, commandPool, nullptr);
       for (auto framebuffer : swapChainFramebuffers) {
         vkDestroyFramebuffer(device, framebuffer, nullptr);
