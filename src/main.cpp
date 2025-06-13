c07ce5dbd8ba0c63571c2442d451a3ee1bcd5655
findeevy <findeevy@gmail.com>
2025-06-12 20:42:17 -0500

    index buffer added

--- src/main.cpp
+++ src/main.cpp
@@ -97,9 +97,14 @@ struct Vertex {
 };
 
 const std::vector<Vertex> vertices = {
-  {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-  {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-  {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+  {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+  {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+  {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+  {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}
+};
+
+const std::vector<uint16_t> indices = {
+  0, 1, 2, 2, 3, 0
 };
 
 class HelloTriangleApplication{
@@ -143,6 +148,9 @@ class HelloTriangleApplication{
 
     VkBuffer vertexBuffer;
     VkDeviceMemory vertexBufferMemory;
+    VkBuffer indexBuffer;
+    VkDeviceMemory indexBufferMemory;
+
 
     uint32_t currentFrame = 0;
     bool framebufferResized = false;
@@ -379,6 +387,7 @@ class HelloTriangleApplication{
       createFramebuffers();
       createCommandPool();
       createVertexBuffer();
+      createIndexBuffer();
       createCommandBuffer();
       createSyncObjects();
     }
@@ -449,6 +458,25 @@ class HelloTriangleApplication{
       vkQueueWaitIdle(graphicsQueue);
 
       vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
+    }
+
+    void createIndexBuffer(){
+      VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+      VkBuffer stagingBuffer;
+      VkDeviceMemory stagingBufferMemory;
+      createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+      void* data;
+      vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+      memcpy(data, indices.data(), (size_t) bufferSize);
+      vkUnmapMemory(device, stagingBufferMemory);
+
+      createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+      copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+      vkDestroyBuffer(device, stagingBuffer, nullptr);
+      vkFreeMemory(device, stagingBufferMemory, nullptr);
     }
 
     void createVertexBuffer() {
@@ -522,8 +550,8 @@ class HelloTriangleApplication{
       VkBuffer vertexBuffers[] = {vertexBuffer};
       VkDeviceSize offsets[] = {0};
       vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
-      vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1,
-0, 0);     
+      vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+      vkCmdDrawIndexed(commandBuffer, static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
 
  
       //Initialize the viewport that we are going to be displaying in (using the info from our swap chain).
@@ -1079,6 +1107,10 @@ class HelloTriangleApplication{
 
     void cleanup(){
       cleanupSwapChain();
+
+      vkDestroyBuffer(device, indexBuffer, nullptr);
+      vkFreeMemory(device, indexBufferMemory, nullptr);
+
       vkDestroyBuffer(device, vertexBuffer, nullptr);
       vkFreeMemory(device, vertexBufferMemory, nullptr);
       for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) { 
