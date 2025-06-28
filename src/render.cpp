20f3c589ff8d532b09008fc4cb2fca1c38b44201
findeevy <findeevy@gmail.com>
2025-06-28 01:44:04 -0500

    fixed cleanup

--- src/render.cpp
+++ src/render.cpp
@@ -1542,49 +1542,44 @@ void FineNgine::updateUniformBuffer(uint32_t currentImage){
 void FineNgine::cleanup(){
   cleanupSwapChain();
 
-  vkDestroySampler(device, textureSampler, nullptr);
-
-  vkDestroyImageView(device, textureImageView, nullptr);
-
-  vkDestroyImage(device, textureImage, nullptr);
-  vkFreeMemory(device, textureImageMemory, nullptr);
+  vkDestroyPipeline(device, graphicsPipeline, nullptr);
+  vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+  vkDestroyRenderPass(device, renderPass, nullptr);
 
   for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++){
     vkDestroyBuffer(device, uniformBuffers[i], nullptr);
     vkFreeMemory(device, uniformBuffersMemory[i], nullptr);
   }
 
+  vkDestroyDescriptorPool(device, descriptorPool, nullptr);
+  vkDestroySampler(device, textureSampler, nullptr);
+  vkDestroyImageView(device, textureImageView, nullptr);
+
+  vkDestroyImage(device, textureImage, nullptr);
+  vkFreeMemory(device, textureImageMemory, nullptr);
   vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
 
   vkDestroyBuffer(device, indexBuffer, nullptr);
   vkFreeMemory(device, indexBufferMemory, nullptr);
-
   vkDestroyBuffer(device, vertexBuffer, nullptr);
   vkFreeMemory(device, vertexBufferMemory, nullptr);
-  for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++){ 
-vkDestroySemaphore(device, renderFinishedSemaphores[i], nullptr);
+
+  for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++){
+    vkDestroySemaphore(device, renderFinishedSemaphores[i], nullptr);
     vkDestroySemaphore(device, imageAvailableSemaphores[i], nullptr);
     vkDestroyFence(device, inFlightFences[i], nullptr);
   }
-  //Remove all of our data structures from memory and kill our GLFW instance.
-  vkDestroySemaphore(device, renderFinishedSemaphore, nullptr);
-  vkDestroySemaphore(device, imageAvailableSemaphore, nullptr);
-  vkDestroyFence(device, inFlightFence, nullptr);
+
   vkDestroyCommandPool(device, commandPool, nullptr);
-  for (auto framebuffer : swapChainFramebuffers){
-    vkDestroyFramebuffer(device, framebuffer, nullptr);
-  }
-  vkDestroyPipeline(device, graphicsPipeline, nullptr);
-  vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
-  vkDestroyRenderPass(device, renderPass, nullptr);
-  for (auto imageView : swapChainImageViews){
-    vkDestroyImageView(device, imageView, nullptr);
-  }
-  vkDestroySwapchainKHR(device,swapChain,nullptr);
+
   vkDestroyDevice(device, nullptr);
+
   if (enableValidationLayers){
     DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
   }
+
+  vkDestroySurfaceKHR(instance, surface, nullptr);
+  vkDestroyInstance(instance, nullptr);
   glfwDestroyWindow(window);
   glfwTerminate();
 }
